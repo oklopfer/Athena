863f58944fdbb65dbdada9da4bf8a581266a0b67
ook37 <oren+12345@taumoda.com>
2024-12-15 19:27:45 -0500

    quickfix

--- itemshop.py
+++ itemshop.py
@@ -159,7 +159,7 @@ class Athena:
 
         all_items = [
             item for item in raw_items 
-            if "layoutId" in item and item["layoutId"] is not None and not item["layoutId"].startswith("JamTracks") and not "tracks" in item
+            if "layoutId" in item and "layout" in item and item["layoutId"] is not None and not item["layoutId"].startswith("JamTracks") and not "tracks" in item
         ]
         for item in all_items:
             if "layout" in item:
